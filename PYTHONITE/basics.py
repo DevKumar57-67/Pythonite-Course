89da15b54736aa5e9e87ca76d304471aadc148dc
Dev <devanshshukla957@gmail.com>
2026-06-30 02:13:50 +0530

    conditions

---
+++ PYTHONITE/basics.py
@@ -0,0 +1,57 @@
+#Comments in python 
+
+
+#Python syntax
+print("Hello, Pythonite")
+
+print("This is python programming language")
+
+#Adding sum
+print(5+6)
+
+# Variables in Python
+x = 45
+y = 5.6
+sum = x+y
+
+print("the sum is ", sum)
+
+#Input/Output in python 
+name= input("Enter your name: ")
+print("Hello", name, "Welcomee to Python prograsmming language")
+
+# Data Types in Python
+# For finding the data type of the variable we use the type() function
+
+x = 55.7
+print("the data type is ", type(x))
+
+#A basic Calculator in python 
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+sum = x+y
+diff = x-y
+prod = x*y
+quot = x/y 
+mod = x%y
+
+print("The sum is ", sum)
+print("The difference is ", diff)
+print("The product is ", prod)
+print("The quotient is ", quot)
+print("The modulus is ", mod)
+print("The modulus is ", mod)
+
+
+#Conditions in Python 
+#Conditional Statements are used to perform different actions based on different conditions
+
+#Voting Eligibility in Python 
+age = int(input("Enter your age: "))
+if (age >= 18):
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
